f02787c120f951f25a99c51b2a23901824631709
Arun T <arunt98>
2025-11-22 14:04:25 -0500

    terraform

---
+++ multi-account/variables.tf
@@ -0,0 +1,9 @@
+variable  "dev" {
+  type        = string
+  default = "752692907119" ### user account ID
+}
+
+variable  "prod" {
+  type        = string
+  default = "160885265516" ## user account ID
+}
